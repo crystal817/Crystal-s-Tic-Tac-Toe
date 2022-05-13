684a721134723cf08e4610968fe67455222580d1
Crystal <cpeguero833@gmail.com>
2022-05-12 22:05:44 -0400

    crystal game

---
+++ script.js
@@ -0,0 +1,16 @@
+// restButton.addEventListener ('click',gameRest)
+
+function chart () {
+let sqaure1 = document.querySelector (".sqaure1")
+let sqaure2 = document.querySelector (".sqaure2")
+let sqaure3 = document.querySelector (".sqaure3")
+let sqaure4 = document.querySelector (".sqaure4")
+let sqaure5 = document.querySelector (".sqaure5")
+let sqaure6 = document.querySelector (".sqaure6")
+let sqaure7 = document.querySelector (".sqaure7")
+let sqaure8 = document.querySelector (".sqaure8")
+let sqaure9 = document.querySelector (".sqaure9")
+
+}
+
+    
